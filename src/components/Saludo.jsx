f9aaee30abc95dde2e510a0662c05e970bde4ddf
Eynar Alvarez <eynar.j.alvarez.m@gmail.com>
2023-08-03 03:23:58 -0400

    The 'Saludo' component was added and implemented in App.jsx

---
+++ src/components/Saludo.jsx
@@ -0,0 +1,15 @@
+
+function Saludo() {
+    return (
+        <>
+            <h1 className='read-the-docs'>
+                Hello World!
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio ipsum provident suscipit est similique dolore reprehenderit alias? Molestias, accusantium. Deleniti assumenda expedita itaque est in cumque, pariatur sequi alias?
+            </p>
+        </>
+    )
+}
+
+export default Saludo;
