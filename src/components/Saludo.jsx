fb07b60654bcd32116df3f48124a271522f93e99
Eynar Alvarez <eynar.j.alvarez.m@gmail.com>
2023-08-03 04:15:31 -0400

    The 'Saludo' component was modified with interpol elements to execute JavaScript sentences or evaluate conditions with a ternary operator and return something, for example a component

--- src/components/Saludo.jsx
+++ src/components/Saludo.jsx
@@ -1,10 +1,24 @@
 
 function Saludo() {
+    const user = "Miguel Melgarejo"
+
+    const add = (n1, n2) => n1 + n2;
+
     return (
         <>
+            {/* Observese que no se usa 'class' por que es una palabra reservada de JS */}
             <h1 className='read-the-docs'>
-                Hello World!
+                Hello {user}!
             </h1>
+            
+            <h2>{5 + 4}</h2>
+            <h3>{5 > 4 ? "Cinco es mayor que cuatro" : "Cinco NO es mayor que cuatro"}</h3>
+            {10 > 20 ? <h4>Título Uno</h4> : <h4>Título Dos</h4>}
+            {add(52, 6)}
+
+            {/* Observese que no se usa 'for' por que es una palabra reservada de JS */}
+            {/* <label htmlFor="name">Name: </label>
+            <input type="text" id="name" /> */}
             <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio ipsum provident suscipit est similique dolore reprehenderit alias? Molestias, accusantium. Deleniti assumenda expedita itaque est in cumque, pariatur sequi alias?
             </p>
@@ -12,4 +26,4 @@ function Saludo() {
     )
 }
 
-export default Saludo;
+export default Saludo
