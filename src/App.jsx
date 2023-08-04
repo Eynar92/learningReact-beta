f32bf87819ee2744c7f19c9f158fb70333a6c46a
Eynar Alvarez <eynar.j.alvarez.m@gmail.com>
2023-08-03 21:31:29 -0400

    Now we know what props are and how to use them.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
 import './App.css'
-import Saludo from './components/Saludo'
+import Gretting from './components/Gretting'
 
 
 function App() {
-  const age = 32;
+  // const age = 32;
+
+  const usersList = ['Nombre 1', 'Nombre 2', 'Nombre 3']
+  const add = (n1, n2) => n1 + n2;
 
   return (
-    <>
-      {age > 20 ? <Saludo /> : ""}
+    <div>
+
+      <Gretting
+        title="Hola"
+        phoneNumber="12345678"
+        isBigger={true}
+        number={2}
+        users={usersList}
+        add={add}
+      />
+
+      <Gretting
+        title="Hello friend"
+        phoneNumber="10101010"
+        isBigger={false}
+        number={2}
+        users={usersList}
+        add={add}
+      />
+
+      <Gretting
+        title="Cómo es?"
+      />
+
+      {/* {age > 20 ? <Saludo /> : ""} */}
       {/* <Saludo />
       <Saludo />
       <Saludo />
       <Saludo />
       <Saludo />
       <Saludo /> */}
-    </>
+    </div>
   )
 }
 
--- src/components/Gretting.jsx
+++ src/components/Gretting.jsx
@@ -1,24 +1,28 @@
 
-function Saludo() {
-    const user = "Miguel Melgarejo"
+function Gretting({ title, phoneNumber, isBigger, number, users, add }) {
+    // const { title } = props;
+    // const user = "Miguel Melgarejo"
 
-    const add = (n1, n2) => n1 + n2;
+    // const add = (n1, n2) => n1 + n2;
 
     return (
         <>
             {/* Observese que no se usa 'class' por que es una palabra reservada de JS */}
-            <h1 className='read-the-docs'>
+            {/* <h1 className='read-the-docs'>
                 Hello {user}!
             </h1>
             
             <h2>{5 + 4}</h2>
             <h3>{5 > 4 ? "Cinco es mayor que cuatro" : "Cinco NO es mayor que cuatro"}</h3>
             {10 > 20 ? <h4>Título Uno</h4> : <h4>Título Dos</h4>}
-            {add(52, 6)}
+            {add(52, 6)} */}
 
             {/* Observese que no se usa 'for' por que es una palabra reservada de JS */}
             {/* <label htmlFor="name">Name: </label>
             <input type="text" id="name" /> */}
+
+            <h1 className="read-the-docs">{title}</h1>
+            <h2 className="read-the-docs">{isBigger ? phoneNumber : "No tiene número de celular"}</h2>
             <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio ipsum provident suscipit est similique dolore reprehenderit alias? Molestias, accusantium. Deleniti assumenda expedita itaque est in cumque, pariatur sequi alias?
             </p>
@@ -26,4 +30,4 @@ function Saludo() {
     )
 }
 
-export default Saludo
+export default Gretting
